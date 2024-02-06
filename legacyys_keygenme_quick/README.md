71c85e61a1fd9b4e8880bed239f70c33b164e2b6
walkingsk <ForgedSpirit@outlook.com>
2024-02-06 16:24:10 +0800

    Added the 1st one

---
+++ legacyys_keygenme_quick/README.md
@@ -0,0 +1,11 @@
+# legacyys_keygenme_quick
+
+This one is a easy start
+
+- key in XXXX-XXXX-XXXX format
+- strlen must be 14
+- dashes ('-') must appear in the right places
+- as in AAAA-BBBB-CCCC:
+  - AAAA must be digits ('0' - '9')
+  - BBBB must be even for each B
+  - CCCC must be exactly "R3KT"
---
+++ legacyys_keygenme_quick/answer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	char s[16];
+	char *p = s;
+	srand(time(0));
+	
+	*p++ = rand()%10 + '0';
+	*p++ = rand()%10 + '0';
+	*p++ = rand()%10 + '0';
+	*p++ = rand()%10 + '0';
+	*p++ = '-';
+	
+	// printable chars
+	// [0x20, 127)
+	*p++ = (rand()%107 + 0x20) & 0xFE;
+	*p++ = (rand()%107 + 0x20) & 0xFE;
+	*p++ = (rand()%107 + 0x20) & 0xFE;
+	*p++ = (rand()%107 + 0x20) & 0xFE;
+	*p++ = '-';
+	
+	*(unsigned int*)p = 0x544B3352;
+	p += 5;
+	*p = 0;
+	// Write C code here
+	
+	printf(s);
+
+	return 0;
+}
